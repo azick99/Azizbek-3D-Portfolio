3e508674d90a669186f68d3657e919d1845ba1ab
@azick99 <yunusaliyevazizbek80@gmail.com>
2023-04-02 00:08:48 +0200

    hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,11 +26,11 @@ const isMobile = useIsMobile()
           </p>
         </div>
       </div>
-      {isMobile ? (
+      {/* {isMobile ? (
         ''
-      ) : (
+      ) : ( */}
           <ComputersCanvas />
-      )}
+      {/* )} */}
       <div className="absolute xs:bottom-5 bottom-20 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
