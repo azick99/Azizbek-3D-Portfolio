3c1f60ffc3d2371447a0f7727212ea25d8e95df1
azick99 <yunusaliyevazizbek80@gmail.com>
2024-05-22 15:41:40 +0200

    changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,58 +1,10 @@
 import { motion } from 'framer-motion'
-import { useRef, useState } from 'react'
 import { SectionWrapper } from '../hoc'
 import { styles } from '../styles'
 import { slideIn } from '../utils/motion'
 import { EarthCanvas } from './canvas'
-import emailjs from '@emailjs/browser'
 
 const Contact = () => {
-  const formRef = useRef()
-  const [form, setForm] = useState({
-    name: '',
-    email: '',
-    message: '',
-  })
-  const [loading, setLoading] = useState(false)
-
-  const handleChange = (e) => {
-    const { name, value } = e.target
-    setForm({ ...form, [name]: value })
-  }
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    setLoading(true)
-    emailjs
-      .send(
-        'service_oompp4n',
-        'template_42yr1ie',
-        {
-          from_name: form.name,
-          to_name: 'Azizbek',
-          from_email: form.email,
-          to_email: 'azizbekyunusaliev99@gmail.com',
-          message: form.message,
-        },
-        'SHV0mXT5c-fOxeze3'
-      )
-      .then(
-        () => {
-          setLoading(false)
-          alert('Thank You, I will get back to you as soon as possible.')
-
-          setForm({
-            name: '',
-            email: '',
-            message: '',
-          })
-        },
-        (error) => {
-          setLoading(false)
-          console.log(error)
-          alert('Something went wrong')
-        }
-      )
-  }
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
       <motion.div
@@ -62,8 +14,8 @@ const Contact = () => {
         <p className={`${styles.sectionSubText}`}>Get in touch</p>
         <h3 className={`${styles.heroHeadText}`}>Contact..</h3>
         <form
-          ref={formRef}
-          onSubmit={handleSubmit}
+          action="https://formsubmit.co/azizbekyunusaliev99@gmail.com"
+          method="POST"
           className="mt-12 flex flex-col gap-8"
         >
           <label className="flex flex-col">
@@ -71,11 +23,10 @@ const Contact = () => {
             <input
               type="text"
               name="name"
-              value={form.name}
-              onChange={handleChange}
               placeholder="What's your name?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white
                rounded-lg outline-none border-none font-medium"
+              required
             />
           </label>
           <label className="flex flex-col">
@@ -83,11 +34,10 @@ const Contact = () => {
             <input
               type="email"
               name="email"
-              value={form.email}
-              onChange={handleChange}
               placeholder="What's your email?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white
                rounded-lg outline-none border-none font-medium"
+              required
             />
           </label>
           <label className="flex flex-col">
@@ -95,19 +45,14 @@ const Contact = () => {
             <textarea
               rows="7"
               name="message"
-              value={form.message}
-              onChange={handleChange}
               placeholder="What do you want to say?"
               className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white
                rounded-lg outline-none border-none font-medium"
+              required
             />
           </label>
-          <button
-            type="submit"
-            onClick={handleSubmit}
-            className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
-          >
-            {loading ? 'Sending...' : 'Send'}
+          <button className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl">
+            Send
           </button>
         </form>
       </motion.div>
